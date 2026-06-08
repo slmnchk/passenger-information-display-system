1658452cc4d30a5198902e8c151ebd5d19e9b5cf
salman <b.salman765@gmail.com>
2026-06-09 01:42:24 +0200

    init commit

---
+++ src/main/java/org/example/passengerinformationdisplaysystem/PassengerInformationDisplaySystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.passengerinformationdisplaysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerInformationDisplaySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerInformationDisplaySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/passengerinformationdisplaysystem/PassengerInformationDisplaySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.passengerinformationdisplaysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerInformationDisplaySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
