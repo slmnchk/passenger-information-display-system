077c37afb73edaecc666aef9636ab66c5d3d680a
salman <b.salman765@gmail.com>
2026-07-20 13:33:46 +0200

    add: frontend template

---
+++ src/main/java/org/example/passengerinformationdisplaysystem/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package org.example.passengerinformationdisplaysystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
