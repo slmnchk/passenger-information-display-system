e8a7bf7b06f0fa8be3a04fb43c64453b2c140a9c
salman <b.salman765@gmail.com>
2026-07-22 10:35:33 +0200

    add: alert by opening the website

--- pids-frontend/scripts/app.js
+++ pids-frontend/scripts/app.js
@@ -1,10 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    alert('Hinweis: Die Website befindet sich noch in der Entwicklung. Die Backend-Methoden sind noch nicht vollständig implementiert!');
+});
+
 function tabSelected(element, actionType) {
     const isActive = element.classList.contains('active');
 
     const buttons = document.querySelectorAll('.request-body-function-button');
     buttons.forEach(btn => btn.classList.remove('active'));
 
+    const tableContainer = document.getElementById('connections-table-container');
+
     if (!isActive) {
         element.classList.add('active');
+        if (actionType === 'verbindungen') {
+            tableContainer.classList.remove('hidden');
+        } else {
+            tableContainer.classList.add('hidden');
+        }
+    } else {
+        tableContainer.classList.add('hidden');
     }
 }
