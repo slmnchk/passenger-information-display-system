26521067064534c8af0facc6204c3df085fd7576
salman <b.salman765@gmail.com>
2026-08-05 01:18:45 +0200

    change: CORS configuration

--- src/main/java/org/example/passengerinformationdisplaysystem/config/CorsConfig.java
+++ src/main/java/org/example/passengerinformationdisplaysystem/config/CorsConfig.java
@@ -1,15 +1,24 @@
 package org.example.passengerinformationdisplaysystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
-                .allowedMethods("GET", "POST", "DELETE", "PUT");
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://salmanchik.com", "https://salmanchik.com", "http://localhost:3000", "http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
